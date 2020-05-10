25bc50a8b563e6d1c792b2625de15e72c1298b7e
Shrutikabansal <ssb2361998@gmail.com>
2020-05-10 23:35:30 +0530

    longest common subsequence

---
+++ DynamicProgramming/longest_common_subsequence.py
@@ -0,0 +1,59 @@
+'''
+Given two sequences, find the length of longest subsequence present in both of them. Both the strings are of uppercase.
+
+Input:
+First line of the input contains no of test cases  T,the T test cases follow.
+Each test case consist of 2 space separated integers A and B denoting the size of string str1 and str2 respectively
+The next two lines contains the 2 string str1 and str2 .
+
+Output:
+For each test case print the length of longest  common subsequence of the two strings .
+
+Constraints:
+1<=T<=200
+1<=size(str1),size(str2)<=100
+
+Example:
+Input:
+2
+6 6
+ABCDGH
+AEDFHR
+3 2
+ABC
+AC
+
+Output:
+3
+2
+
+Explanation
+LCS for input Sequences “ABCDGH” and “AEDFHR” is “ADH” of length 3.
+
+LCS of "ABC" and "AC" is "AC" of length 2.
+'''
+
+def lcs(m,n,X,Y):
+    '''
+    :param m: length of string X 
+    :param n: length of string Y
+    :param X: string X
+    :param Y: string Y
+    :return: Integer
+    '''
+    # code here
+    dp=[[0 for _ in range (n+1)] for _ in range (m+1)]
+    
+    for i in range (0,m+1):
+        dp[i][0]=0
+    
+    for i in range (0,n+1):
+        dp[0][n]=0
+        
+    for i in range (1, m+1):
+        for j in range (1,n+1):
+            if X[i-1]==Y[j-1]:
+                dp[i][j]=dp[i-1][j-1]+1
+            else:
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+    return dp[m][n]
