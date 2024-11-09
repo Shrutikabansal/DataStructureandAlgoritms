bc2713abaa822ed9150ccddd8c85a0cd1e64bd77
Shrutika Bansal <shrutika.bansal@razorpay.com>
2024-11-09 22:10:41 +0530

    Time: 3 ms (79.25%), Space: 14.7 MB (14.38%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -2,27 +2,21 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         
-        unordered_set<int>s;
-        vector<int>v;
-        int i,j;
-        int n=nums.size();
-        int temp;
-        for(i=0;i<n;i++)
-        {
-            temp=target-nums[i];
-            if(s.find(temp)!=s.end())
-            {
-                v.push_back(i);
-                auto it=find(nums.begin(), nums.end(), temp);
-                v.push_back(it-nums.begin());
-             //   cout<<it-v.begin()+1;
-                sort(v.begin(),v.end());
-               break;
-            }
-            else
-                s.insert(nums[i]);
-                
+    map<int, int>mp;
+    int n = nums.size(), i;
+    vector<int>v;
+
+    for(i=0; i<n; i++){
+        if(mp.find(target - nums[i]) == mp.end()){
+            mp[nums[i]] = i;
+        }else{
+            v.push_back(mp[target - nums[i]]);
+            v.push_back(i);
+            break;
         }
-        return v;
+        
     }
+    return v;
+
+}
 };
