28a86a0ff2cf6ec5c7845317fbfa1ae31fa5b7b7
Shrutika Bansal <shrutika.bansal@razorpay.com>
2024-11-12 18:40:45 +0530

    Time: 67 ms (16.05%), Space: 73.2 MB (71.67%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        int n = nums.size(), i;
+
+        set<int>s;
+        for(i=0;i<n;i++){
+            if(s.count(nums[i]) > 0){
+                return true;
+            }else{
+                s.insert(nums[i]);
+            }
+        }
+
+        return false;
+
+    }
+};
