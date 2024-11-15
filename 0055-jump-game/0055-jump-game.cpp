ebac8a259696888a9c86c081b73495ab90af2be7
Shrutika Bansal <shrutika.bansal@razorpay.com>
2024-11-15 23:45:52 +0530

    Time: 3 ms (32.04%), Space: 51.1 MB (47.13%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -2,35 +2,16 @@ class Solution {
 public:
     bool canJump(vector<int>& nums) {
 
-        int n = nums.size();
-        if(n == 1){
-            return true;
+       int n = nums.size();
+       int target = n-1;
+       int i;
+
+       for(i=n-2;i>=0;i--){
+        if(i+nums[i] >= target){
+            target = i;
         }
+       }
 
-        if (nums[0] == 0){
-            return false;
-        }
-
-
-
-        vector<int> jump(n, INT_MAX);
-        jump[0] = 0;
-
-        int i,j;
-
-        for(i=1;i<n;i++){
-            for(j=0;j<i;j++){
-                if(i<=j+nums[j] && jump[j]!=INT_MAX){
-                    jump[i] = min(jump[i], jump[j]+1);
-                }
-            }
-        }
-
-        if(jump[n-1]!=INT_MAX){
-            return true;
-        }
-        return false;
-
-        
+       return target ==0;
     }
 };
