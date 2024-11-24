8de0473e9e0003e4b6f7bbe3ee5882a98f11f4f2
Shrutika Bansal <shrutika.bansal@razorpay.com>
2024-11-24 13:50:33 +0530

    Time: 19 ms (10.97%), Space: 11.2 MB (5.12%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        map<char, int> mp;
+
+        int n = s.size();
+        int i =0 ,j, maxFreq;
+        int ans = 0;
+
+        for(j=0;j<n;j++){
+            mp[s[j]] ++;
+            maxFreq = max(maxFreq, mp[s[j]]);
+            if(j-i+1-maxFreq > k){
+                mp[s[i]]--;
+
+                ++i;
+            }else{
+            ans = max(ans, j-i+1);
+
+            }
+
+
+
+        }
+        return ans;
+    }
+};
